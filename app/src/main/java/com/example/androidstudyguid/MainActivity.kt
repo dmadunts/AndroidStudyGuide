3eeffe9ea89931827d46cbca160e057418bcf425
dmadunts <madunts.d@gmail.com>
2022-02-15 15:46:11 +0500

    Added RecyclerView and NavigationComponent for #4. Amend: fix for Detekt lint check

--- app/src/main/java/com/example/androidstudyguid/MainActivity.kt
+++ app/src/main/java/com/example/androidstudyguid/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.androidstudyguid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.example.androidstudyguid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigationComponent()
+    }
+
+    private fun setupNavigationComponent() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val graphInflater = navController.navInflater
+        val navGraph = graphInflater.inflate(R.navigation.nav_graph)
+
+        navGraph.setStartDestination(R.id.articleListFragment)
+        navController.graph = navGraph
     }
 }
---
+++ app/src/main/java/com/example/androidstudyguid/models/Article.kt
@@ -0,0 +1,8 @@
+package com.example.androidstudyguid.models
+
+data class Article(
+    val id: Int,
+    val title: String,
+    val authorName: String,
+    val url: String,
+)
