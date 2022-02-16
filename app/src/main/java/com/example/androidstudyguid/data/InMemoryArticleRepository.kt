d4e4147d7777fcff70f14d30357a22e177d5d6ef
dmadunts <madunts.d@gmail.com>
2022-02-16 13:43:13 +0500

    Minor fixes

--- app/src/main/java/com/example/androidstudyguid/data/InMemoryArticleRepository.kt
+++ app/src/main/java/com/example/androidstudyguid/data/InMemoryArticleRepository.kt
@@ -2,19 +2,25 @@ package com.example.androidstudyguid.data
 
 import com.example.androidstudyguid.articlelist.ArticleListFragment
 import com.example.androidstudyguid.models.Article
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.withContext
 
-class InMemoryArticleRepository : ArticleRepository {
+class InMemoryArticleRepository(private val dispatcher: CoroutineDispatcher = IO) :
+    ArticleRepository {
     override suspend fun fetchArticles(): List<Article> {
-        return arrayListOf<Article>().apply {
-            for (i in 1..ArticleListFragment.DEFAULT_ITEM_COUNT) {
-                add(
-                    Article(
-                        i,
-                        "Title $i",
-                        "Author $i",
-                        "https://github.com/dmadunts/AndroidStudyGuide"
+        return withContext(dispatcher) {
+            arrayListOf<Article>().apply {
+                for (i in 1..ArticleListFragment.DEFAULT_ITEM_COUNT) {
+                    add(
+                        Article(
+                            i,
+                            "Title $i",
+                            "Author $i",
+                            "https://github.com/dmadunts/AndroidStudyGuide"
+                        )
                     )
-                )
+                }
             }
         }
     }
