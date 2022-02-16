e782dfd954b2d8a77391598b1911be65f3f64e21
dmadunts <madunts.d@gmail.com>
2022-02-16 17:34:43 +0500

    Added Suppress option to fix lint error

--- app/src/main/java/com/example/androidstudyguid/data/remote/androidessence/AndroidEssenceAPI.kt
+++ app/src/main/java/com/example/androidstudyguid/data/remote/androidessence/AndroidEssenceAPI.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.androidstudyguid.data.remote.androidessence
 
 import com.example.androidstudyguid.data.remote.androidessence.models.AndroidEssenceFeed
