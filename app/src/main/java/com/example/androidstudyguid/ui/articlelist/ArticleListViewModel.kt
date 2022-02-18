23eca342972827f66920cc7dbcb1ab335605639e
dmadunts <madunts.d@gmail.com>
2022-02-18 17:15:35 +0500

    Added Loading state for #14

--- app/src/main/java/com/example/androidstudyguid/ui/articlelist/ArticleListViewModel.kt
+++ app/src/main/java/com/example/androidstudyguid/ui/articlelist/ArticleListViewModel.kt
@@ -4,17 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.androidstudyguid.data.models.ui.Article
 import com.example.androidstudyguid.data.repositories.interfaces.ArticleRepository
 import kotlinx.coroutines.launch
 
 class ArticleListViewModel(articleRepository: ArticleRepository) : ViewModel() {
-    private val _articles = MutableLiveData<List<Article>>()
-    val articles: LiveData<List<Article>> = _articles
+    private val _state = MutableLiveData<ArticleListViewState>()
+    val state: LiveData<ArticleListViewState> = _state
 
     init {
         viewModelScope.launch {
-            _articles.value = articleRepository.fetchArticles()
+            _state.value = ArticleListViewState.loading()
+            try {
+                val articles = articleRepository.fetchArticles()
+                _state.value = ArticleListViewState.success(articles)
+            } catch (e: Exception) {
+                _state.value = ArticleListViewState.error(e)
+            }
         }
     }
 }
