d5176c61f7649f1d2e895042ff7bbb06e909aa78
dmadunts <madunts.d@gmail.com>
2022-03-29 13:45:27 +0500

    Add unit test for ArticleListViewModelTest.kt (#20)
    * Add unit test for ArticleListViewModelTest.kt
    
    * Lint fixes

---
+++ app/src/test/java/com/example/androidstudyguid/ui/articlelist/ArticleListViewModelTest.kt
@@ -0,0 +1,31 @@
+package com.example.androidstudyguid.ui.articlelist
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.androidstudyguid.data.models.ui.Article
+import com.example.androidstudyguid.fakes.CoroutinesTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class ArticleListViewModelTest {
+    private val testRobot = ArticleListViewModelRobot()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutinesTestRule = CoroutinesTestRule()
+
+    @Test
+    fun successfulRequest() {
+        val testArticles = listOf(
+            Article("1", "title", "author", "url")
+        )
+
+        testRobot
+            .mockArticles(testArticles)
+            .buildViewModel()
+            .assertViewState(ArticleListViewState.success(testArticles))
+    }
+}
